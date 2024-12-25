0b3fe1d0e95ecf5408a5fbe9dbb9c816d469fdb7
Daniel Vega <lyradeperseo@gmail.com>
2024-12-24 23:40:14 -0500

    Create debounce_leading.js

---
+++ debounce_leading.js
@@ -0,0 +1,12 @@
+function debounce_leading(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+    }, timeout);
+  };
+}
